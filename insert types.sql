8a799e61e0ce8edb3ad3f1d31053960b8e368b33
Diogo Sousa <42503667+Diogo12246@users.noreply.github.com>
2018-12-07 22:02:20 +0000

    uploaded testing DB (buggy_notfinal)

---
+++ insert types.sql
@@ -0,0 +1,20 @@
+insert into planettype(Type)
+values ('Double planet');
+
+insert into planettype(Type)
+values ('Dwarf planet');
+
+insert into planettype(Type)
+values ('Exoplanet');
+
+insert into planettype(Type)
+values ('Extragalactic planet');
+
+insert into planettype(Type)
+values ('Goldilocks planet');
+
+insert into planettype(Type)
+values ('Pulsar planet');
+
+insert into planettype(Type)
+values ('Hypothetical Planet')
